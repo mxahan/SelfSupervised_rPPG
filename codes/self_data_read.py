606fe3df8e1f6402db78590574db5a3af8a66489
mxahan <smxahsan@gmail.com>
2023-03-17 00:13:50 -0400

    new

--- codes/self_data_read.py
+++ codes/self_data_read.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
 Created on Thu Apr 23 10:06:57 2020
 
 @author: zahid
-"""afa
+"""
 
-#%% libraries
+
+#%% librariesa
 
 import tensorflow as tf
 
@@ -18,8 +20,7 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 import matplotlib.pyplot as plt
 
 import numpy as np
-
-import cv2
+# import cv2
 
 import glob
 
@@ -97,7 +98,7 @@ def data_read(files, im_size = (200, 200)):
     
     return data
 
-data = data_read(files, im_size = (200, 200) )
+# data = data_read(files, im_size = (200, 200) )
 
 
 #%% PPG signal selection and alignment. 
@@ -148,21 +149,17 @@ def get_pkl_files():
     list.sort(files) # serialing the data
     return files 
 
-files = get_pkl_files()
-
 
 input("pickle load ahead")
 
 def load_pickle(save_path):
-
-
     with open(save_path, 'rb') as f:
         a,b = pickle.load(f)
         return a, b
 
 
 def get_data(files):
-    for i in [4]:
+    for i in [2]:
         data_align, ppgnp_align= load_pickle(save_path= files[i])
         if 'data_a' in locals():
             data_a = np.append(data_a, data_align, axis = 0)
@@ -173,6 +170,9 @@ def get_data(files):
         
     return data_a, ppg_a
 
+
+
+files = get_pkl_files()
 data_align, ppgnp_align =  get_data(files)
 
 
@@ -195,13 +195,19 @@ class data_loader():
         if pos==None:
             pos = randint(50, self.max_len)
         img = np.transpose(self.video[pos:pos+self.frame_cons*frame_gap:frame_gap,:,:],[1,2,0])
-        p_point = np.int(np.round(pos*64/30))
-        ppg_gt = self.ppg[p_point: p_point+85*frame_gap: frame_gap, 0]
         
-        ppg_gt = ppg_gt-np.min(ppg_gt)
         
-        ppg_gt = (ppg_gt/np.max(ppg_gt))*2 -1
         
+        if self.ppg.any() != None:
+            p_point = np.int(np.round(pos*64/30))
+            ppg_gt = self.ppg[p_point: p_point+85*frame_gap: frame_gap, 0]
+            
+            ppg_gt = ppg_gt-np.min(ppg_gt)
+            
+            ppg_gt = (ppg_gt/np.max(ppg_gt))*2 -1
+        else: 
+            ppg_gt = None
+            
         if reSiz:
             img =  self.img_resize(img)
         
@@ -211,10 +217,31 @@ class data_loader():
         vs, gt = [], []
         if bs ==None: bs =self.bs
         for i in range(bs):
-            frame_gap= 1 if random.random()>0.90 else 2
+            frame_gap= 1 if random.random()<0.90 else 2
             sas, gtss, _ =  self.get_sup_samp(frame_gap=frame_gap)
             vs.append(sas); gt.append(gtss)
         return tf.cast(tf.stack(vs), tf.float32)/255.0, tf.cast(tf.stack(gt), tf.float32)
+    
+    def get_pair_supD(self):
+        vs, gt = [], []
+        pos = randint(40, self.max_len)
+        sas, gtss, _ =  self.get_sup_samp(pos = pos, frame_gap=1)
+        vs.append(sas); gt.append(gtss)
+        
+        frame_gap= 1 if random.random()<0.90 else 2
+        
+        if frame_gap==2:
+            s_ = 0
+            sas, gtss, _ =  self.get_sup_samp(pos = pos, frame_gap=frame_gap)
+        else:
+            s_ = randint(5,25)
+            sas, gtss, _ =  self.get_sup_samp(pos = pos+s_, frame_gap=frame_gap)
+        vs.append(sas); gt.append(gtss)
+        
+        return tf.cast(tf.stack(vs), tf.float32)/255.0, tf.cast(tf.stack(gt), tf.float32), s_
+        
+        
+        
     
     def rand_frame_shuf(self, img):
         img = img.numpy()
@@ -309,19 +336,48 @@ class Contrastive_loss(tf.keras.layers.Layer):
 
 # triplet/max margin loss
 
+
+#%% Discriminator losses
+# This method returns a helper function to compute cross entropy loss
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def generator_loss(fake_output):
+    return 0.5*cross_entropy(tf.ones_like(fake_output), fake_output)
+
+def shifted_loss(op_1, op_2, s_):
+    if s_>0:
+        s_ = np.int(np.round(s_*64/30))
+        losss=tf.keras.losses.MSE(y_true = op_1[:, s_:85], y_pred = op_2[:, 0:(85-s_)])
+    else:
+        losss=tf.keras.losses.MSE(y_true = op_1[:, 0:85:2], y_pred = op_2[:, 0:43])
+    return losss
+
 #%% Network Definition 
 
-from net_work_def import CNN_back, MtlNetwork_head
+from net_work_def import CNN_back, MtlNetwork_head, PPG_discriminator, PPG_generator
+
 #%% 
+
 def get_network(head = 85):
     
     body = CNN_back()
-    proj_head = MtlNetwork_head(head)
+    # proj_head = MtlNetwork_head(head)
+    proj_head = PPG_generator()
     neural_net = tf.keras.Sequential([body, proj_head])
     
     return neural_net
 
-neural_net =  get_network(60)
+neural_net =  get_network(85)
+
+ppg_disc = PPG_discriminator()
+ppg_gen = neural_net
+
+# https://www.tensorflow.org/guide/keras/transfer_learning
 
 #%% Supervised Loss 
 def RootMeanSquareLoss1(y,x):
@@ -333,32 +389,43 @@ def RootMeanSquareLoss1(y,x):
     # print(loss2.shape)
     
     # print(tf.reduce_mean(loss), tf.reduce_mean(loss2))
-    return loss + 0.5*loss2
+    return tf.reduce_mean(loss + 0.5*loss2)
 
 
 #%% Optimization 
 
+# loss_crit = Contrastive_loss(tau=0.05)
 loss_crit = Contrastive_loss(tau=0.05)
 
 optimizer= tf.keras.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
 
-optimizer1  = tf.optimizers.SGD(learning_rate=0.0009)
+optimizer1  = tf.optimizers.SGD(learning_rate=0.0005)
+
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
 loss_v = []
 
 # give a zero loss to the frame repeating
 
 
-def wt_save(neural_net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Models/ssl_model'):
+def wt_save(neural_net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Models/ssl_model_2'):
     neural_net.save_weights(save_mdl)
 
 def trainNetProb(net):
     samp_load =  data_loader(video=data_align, ppg = ppgnp_align, bs = 1)
-    for step in range(30000):
-        x = samp_load.get_CL_data(pve = 1, nve = 8)
-        with tf.GradientTape() as g:
-            pred =  net(x, training = True) 
-            loss =  loss_crit(pred[0:1],pred[1:])  
+    for step in range(100000):
+        if random.random() <0.9:
+            x,y = samp_load.get_sup_data(bs = 8)
+            with tf.GradientTape() as g:
+                pred =  net(x, training = True) 
+                loss =  RootMeanSquareLoss1(y,pred)  
+        else:
+            x,y, s_ = samp_load.get_pair_supD()
+            with tf.GradientTape() as g:
+                pred =  net(x, training = True) 
+                loss =  shifted_loss(pred[0:1], pred[1:2], s_) 
+            
         trainable_variables =  net.trainable_variables
         gradients =  g.gradient(loss, trainable_variables)  
         optimizer1.apply_gradients(zip(gradients, trainable_variables))
@@ -368,14 +435,62 @@ def trainNetProb(net):
             print("step %i, loss: %f" %(step, loss))
             
         if step % (9999) == 0:
+            wt_save(net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Models/ssl_mods_2')
+
+
+def train_gen_model():
+    samp_load =  data_loader(video=data_align, ppg = ppgnp_align, bs = 1)
+    for step in range(3000):
+        
+        x = samp_load.get_CL_data(pve = 1, nve = 8)
+        with tf.GradientTape() as g:
+            pred =  neural_net(x, training = True) 
+            loss =  loss_crit(pred[0:1],pred[1:])  
+        trainable_variables =  neural_net.trainable_variables
+        gradients =  g.gradient(loss, trainable_variables)  
+        optimizer1.apply_gradients(zip(gradients, trainable_variables))
+        
+        if step % (500) == 0:
+            loss_v.append(loss)
+            print("step %i, loss: %f" %(step, loss))
             
-            wt_save(net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Models/ssl_mods')
+        
+        x_feed, targ_real = samp_load.get_sup_data(bs = 6)
+        # x_feed = tf.expand_dims(tf.stop_gradient(neural_net(x)), axis= 1)
+        # x_feed = tf.stop_gradient(neural_net(x))
+        targ_real = tf.expand_dims(targ_real, axis = 2)
+        
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            # gened_ppg = ppg_gen(x_feed, training= True)
+            gened_ppg = ppg_gen(x_feed, training= True)
+            gened_ppg = tf.expand_dims( gened_ppg, axis = 2)
+            real_output = ppg_disc(targ_real, training=True)
+            fake_output = ppg_disc(gened_ppg, training=True)
+            gen_loss = generator_loss(fake_output)
+            if step%5 ==0:
+                disc_loss = discriminator_loss(real_output, fake_output)
+ 
+        gradients_of_generator = gen_tape.gradient(gen_loss, ppg_gen.trainable_variables)
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, ppg_gen.trainable_variables))
+        
+        if step%5 ==0:
+            gradients_of_discriminator = disc_tape.gradient(disc_loss, ppg_disc.trainable_variables)
+            discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, ppg_disc.trainable_variables))
+        
+        if step % (500) == 0:
+            # loss_v.append(loss)
+            print("step %i, loss: %f, loss: %f" %(step, gen_loss, disc_loss))
             
+        # if step % (9999) == 0:
+#            wt_save(net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Models/ssl_mods')            
             
 #%% Training main
 
 with tf.device('gpu:0'): #very important
     trainNetProb(neural_net)
+    
+# with tf.device('gpu:0'): #very important
+#     train_gen_model()
 
     
 # wt_save(neural_net)
@@ -387,3 +502,61 @@ def wt_load(neural_net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Mod
     return neural_net
     
 neural_net =  wt_load(neural_net, save_mdl = '../../../Dataset/Merl_Tim/NNsave/SavedWM/Models/ssl_mods')
+
+#%% data Plot
+def data_plot(data, rs = 500, rw = 5, save_Fig = 0):
+    opt = []
+    for pos in list(range(rs, rs +rw*40, 40)):
+        img = np.transpose(data[pos:pos+40:1,:,:],[1,2,0])
+        img = tf.image.resize(img, (100, 100))/255.0
+        opt.append(neural_net(img))
+        
+    a = np.array(opt).ravel().shape
+    fig = plt.figure(figsize=(19.20*2,10.80*2))
+    ax1 = fig.add_subplot()
+    plt.plot(list(np.arange(0, a[0]/64,1/64)), np.array(opt).ravel(), linewidth=5)
+    plt.ylabel('PPG (Normalized Voltage)', fontweight="bold", fontsize=35)
+    plt.xlabel('Time (second)', fontweight="bold", fontsize=35)
+    # plt.title('PPG plot',fontsize=45, fontweight="bold")
+    plt.yticks(fontsize=35)
+    plt.xticks(fontsize=35)
+    plt.grid(visible=True, color='k', linestyle='-', linewidth=0.5)
+
+    if save_Fig:
+        plt.savefig('video_res.png', bbox_inches="tight", format = 'png', dpi= 500)
+    plt.show()
+    return np.array(opt).ravel()
+
+def good_plot(x, y, x_label = 'Time (second)', y_label= 'PPG (Normalized Voltage)',  
+              title = None, save_Fig = 0):
+    
+    fig = plt.figure(figsize=(19.20*2,10.80*2))
+    ax1 = fig.add_subplot()
+    plt.plot(x, y, linewidth=5)
+    plt.ylabel(x_label, fontweight="bold", fontsize=35)
+    plt.xlabel(y_label, fontweight="bold", fontsize=35)
+    # plt.title('PPG plot',fontsize=45, fontweight="bold")
+    plt.yticks(fontsize=35)
+    plt.xticks(fontsize=35)
+    plt.grid(visible=True, color='k', linestyle='-', linewidth=0.5)
+
+    if save_Fig:
+        plt.savefig('video_res.png', bbox_inches="tight", format = 'png', dpi= 500)
+    plt.show()
+    
+    
+
+#%% Frequency Domain analysis
+
+from scipy.fft import fft, fftfreq
+
+def freq_analysis(x):
+    if len(x.shape)>1:
+        print("Check the array shape. Should be one dimensional")
+    yf = fft(x)
+    xf = fftfreq(len(x), 1 / 64)
+
+    # plt.plot(xf[0:int(len(x)/8)]*60, np.abs(yf)[0:int(len(x)/8)])
+    good_plot(x = xf[0:int(len(x)/8)]*60, y =  np.abs(yf)[0:int(len(x)/8)])
+    plt.show() 
+    return 
